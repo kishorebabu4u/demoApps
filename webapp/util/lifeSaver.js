f74d5fd6973e09db2cf9005d00b476e7494eca38
Kishore Babu <kishorebabu4u@gmail.com>
2021-09-16 07:06:37 +0000

    commit

---
+++ webapp/util/lifeSaver.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    
+], function() {
+    'use strict';
+    return {
+        convertName: function(inp){
+            if(inp){
+                return inp.toUpperCase();
+            }
+        }
+    };
+});
